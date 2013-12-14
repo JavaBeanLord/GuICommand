2a1221a6562171ba4a66ff01a058dc50e34420c5
JavaBeanLord <justinrgarr@zonecomputers.org>
2013-12-13 22:52:20 -0500

    Fixed Upload Issue / Ready To Run App
    Pre Build / Command Prompt GuI Style (iCandy)

---
+++ Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.OutputTextBox = New System.Windows.Forms.TextBox()
+        Me.ExecuteButton = New System.Windows.Forms.Button()
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(484, 36)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(180, 366)
+        Me.Panel1.TabIndex = 0
+        '
+        'OutputTextBox
+        '
+        Me.OutputTextBox.Location = New System.Drawing.Point(12, 36)
+        Me.OutputTextBox.Multiline = True
+        Me.OutputTextBox.Name = "OutputTextBox"
+        Me.OutputTextBox.ReadOnly = True
+        Me.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.OutputTextBox.Size = New System.Drawing.Size(466, 366)
+        Me.OutputTextBox.TabIndex = 1
+        '
+        'ExecuteButton
+        '
+        Me.ExecuteButton.Location = New System.Drawing.Point(484, 408)
+        Me.ExecuteButton.Name = "ExecuteButton"
+        Me.ExecuteButton.Size = New System.Drawing.Size(180, 19)
+        Me.ExecuteButton.TabIndex = 3
+        Me.ExecuteButton.Text = "Execute"
+        Me.ExecuteButton.UseVisualStyleBackColor = True
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Location = New System.Drawing.Point(12, 408)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(466, 20)
+        Me.InputTextBox.TabIndex = 2
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ToolsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(676, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CloseToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ToolsToolStripMenuItem
+        '
+        Me.ToolsToolStripMenuItem.Name = "ToolsToolStripMenuItem"
+        Me.ToolsToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.ToolsToolStripMenuItem.Text = "Tools"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(4, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(173, 50)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Digital-7 Mono", 9.749999!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 23)
+        Me.Label1.MaximumSize = New System.Drawing.Size(173, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 14)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Timer1
+        '
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Digital-7 Mono", 9.749999!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 79)
+        Me.Label2.MaximumSize = New System.Drawing.Size(173, 14)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 14)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(4, 60)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(173, 50)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Digital-7 Mono", 9.749999!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(13, 135)
+        Me.Label3.MaximumSize = New System.Drawing.Size(173, 14)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 14)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(4, 116)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(173, 50)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.ExecuteButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(676, 439)
+        Me.Controls.Add(Me.ExecuteButton)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Controls.Add(Me.OutputTextBox)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(692, 473)
+        Me.MinimumSize = New System.Drawing.Size(692, 473)
+        Me.Name = "Form1"
+        Me.Text = "EasyCom"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents OutputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ExecuteButton As System.Windows.Forms.Button
+    Friend WithEvents InputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Public Class Form1
+    Private WithEvents MyProcess As Process
+    Private Delegate Sub AppendOutputTextDelegate(ByVal text As String)
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Timer1.Enabled = True
+
+        Me.AcceptButton = ExecuteButton
+        MyProcess = New Process
+        With MyProcess.StartInfo
+            .FileName = "CMD.EXE"
+            .UseShellExecute = False
+            .CreateNoWindow = True
+            .RedirectStandardInput = True
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+        End With
+        MyProcess.Start()
+
+        MyProcess.BeginErrorReadLine()
+        MyProcess.BeginOutputReadLine()
+        AppendOutputText("Process Started at: " & MyProcess.StartTime.ToString)
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        MyProcess.StandardInput.WriteLine("EXIT") 'send an EXIT command to the Command Prompt
+        MyProcess.StandardInput.Flush()
+        MyProcess.Close()
+    End Sub
+
+    Private Sub MyProcess_ErrorDataReceived(ByVal sender As Object, ByVal e As System.Diagnostics.DataReceivedEventArgs) Handles MyProcess.ErrorDataReceived
+        AppendOutputText(vbCrLf & "Error: " & e.Data)
+    End Sub
+
+    Private Sub MyProcess_OutputDataReceived(ByVal sender As Object, ByVal e As System.Diagnostics.DataReceivedEventArgs) Handles MyProcess.OutputDataReceived
+        AppendOutputText(vbCrLf & e.Data)
+    End Sub
+
+    Private Sub ExecuteButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExecuteButton.Click
+        If InputTextBox.Text = Nothing Then
+            MyProcess.StandardInput.WriteLine("chkdsk")
+            MyProcess.StandardInput.Flush()
+            InputTextBox.Text = ""
+            My.Settings.exme = ("chkdsk")
+        Else
+            MyProcess.StandardInput.WriteLine(InputTextBox.Text)
+            MyProcess.StandardInput.Flush()
+            My.Settings.exme = InputTextBox.Text
+            InputTextBox.Text = ""
+        End If
+
+    End Sub
+    ''had to add catch
+    Private Sub AppendOutputText(ByVal text As String)
+        Try
+            If OutputTextBox.InvokeRequired Then
+                Dim myDelegate As New AppendOutputTextDelegate(AddressOf AppendOutputText)
+                Me.Invoke(myDelegate, text)
+            Else
+                OutputTextBox.AppendText(text)
+            End If
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If InputTextBox.Text = Nothing Then
+            PictureBox1.BackColor = Color.LawnGreen
+            Label1.BackColor = Color.LawnGreen
+            Label1.Text = ("Ready To Scan HardDisk!")
+        Else
+            PictureBox1.BackColor = Color.CornflowerBlue
+            Label1.BackColor = Color.CornflowerBlue
+            Label1.Text = ("Ready To: ") & InputTextBox.Text.ToString
+
+        End If
+
+
+        If My.Settings.exme = Nothing Then
+            PictureBox2.BackColor = Color.Yellow
+            Label2.BackColor = Color.Yellow
+            Label2.Text = ("Last Execute Not Found!")
+        Else
+            PictureBox2.BackColor = Color.CornflowerBlue
+            Label2.BackColor = Color.CornflowerBlue
+            Label2.Text = ("Last Execute: ") & My.Settings.exme
+        End If
+
+        If InputTextBox.Text = Nothing Then
+            Label3.BackColor = Color.Red
+            PictureBox3.BackColor = Color.Red
+            Label3.Text = ("Execute Will Run CHKDSK!")
+        Else
+            Label3.BackColor = Color.LawnGreen
+            PictureBox3.BackColor = Color.LawnGreen
+            Label3.Text = ("Execute Will Run Input!")
+        End If
+
+
+    End Sub
+End Class
